f7289937019b24c8ef9432fb766c899155ade11b
Anjo Tadena <tadena.anjo@gmail.com>
2021-01-01 18:15:48 +0800

    Configure eslinter (WIP)

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['detox', 'jest', 'prettier'],
+  env: {
+    es2021: true,
+    'detox/detox': true,
+    'jest/globals': true,
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true, optionalDependencies: false, peerDependencies: false }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'func-names': 0,
+  },
+};
