5dbfb17d89b2d40fc2e2f0bc0c2f187f4dda9982
Anjo Tadena <tadena.anjo@gmail.com>
2021-01-01 18:21:33 +0800

    Add smoke test sample

---
+++ __tests__/smoke.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { shallow } from 'enzyme';
+
+describe('Text', () => {
+  it('renders text', () => {
+    const wrapper = shallow(<Text>Hello, World!</Text>);
+
+    expect(wrapper.text()).toEqual('Hello, World!');
+  });
+});
