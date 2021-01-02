6fe6f5ad88901aebc64579a0f1e4d613d86b1526
Anjo Tadena <tadena.anjo@gmail.com>
2021-01-02 11:27:02 +0800

    Fix quotes eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,31 @@
 module.exports = {
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ["plugin:react/recommended", "airbnb"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: ['detox', 'jest', 'prettier'],
+  plugins: ["detox", "jest", "prettier"],
   env: {
     es2021: true,
-    'detox/detox': true,
-    'jest/globals': true,
+    "detox/detox": true,
+    "jest/globals": true,
   },
   rules: {
-    'react/prop-types': 'off',
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true, optionalDependencies: false, peerDependencies: false }],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'func-names': 0,
+    "react/prop-types": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "func-names": 0,
+    quotes: ["error", "double"],
+    "jsx-quotes": ["error", "single"],
   },
 };
